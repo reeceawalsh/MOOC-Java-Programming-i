aae8188a99788b9a2ed81b4e08ddde9e62d3c73a
reeceawalsh <reeceawalsh@hotmail.co.uk>
2022-05-04 22:02:26 +0100

    Part 5 complete

--- part01-Part01_05.Message/src/main/java/Message.java
+++ part01-Part01_05.Message/src/main/java/Message.java
@@ -4,10 +4,13 @@ import java.util.Scanner;
 public class Message {
 
     public static void main(String[] args) {
+        // Creates a tool for reading input
         Scanner scanner = new Scanner(System.in);
-
+        // Prints "write a message"
         System.out.println("Write a message:");
-        // Write your program here
-
+        // Reads the string written by the user and assign it to program memory. The string message = what ever the use input
+        String message = scanner.nextLine();
+        // Prints out the message variable which is the users input
+        System.out.println(message);
     }
 }
