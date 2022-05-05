f1244a32145ba11a9e207d67ce414978bf14b8e8
reeceawalsh <reeceawalsh@hotmail.co.uk>
2022-05-05 18:23:29 +0100

    Part 21 completed

--- part01-Part01_21.AverageOfTwoNumbers/src/main/java/AverageOfTwoNumbers.java
+++ part01-Part01_21.AverageOfTwoNumbers/src/main/java/AverageOfTwoNumbers.java
@@ -6,7 +6,11 @@ public class AverageOfTwoNumbers {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        // Write your program here
-
+        System.out.println("Give the first number:");
+        int firstNum = Integer.valueOf(scanner.nextLine());
+        System.out.println("Give the second number:");
+        int secondNum = Integer.valueOf(scanner.nextLine());
+        double result = (double) (firstNum + secondNum) / 2;
+        System.out.println("The average is " + (result));
     }
 }
