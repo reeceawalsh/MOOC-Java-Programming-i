152bb6dcc12faf79951f82f402c4d6154ef3b8f5
reeceawalsh <reeceawalsh@hotmail.co.uk>
2022-05-05 11:48:59 +0100

    Part 9 and 10 completed

--- part01-Part01_01.Sandbox/src/main/java/Sandbox.java
+++ part01-Part01_01.Sandbox/src/main/java/Sandbox.java
@@ -3,6 +3,11 @@ public class Sandbox {
 
     public static void main(String[] args) {
 
-        System.out.println("Hello");
-    }
+        System.out.println("Greetings! How are you doing?");
+        String reply1 = scanner.nextLine();
+        System.out.println("Oh, how interesting. Tell me more!");
+        String reply2 = scanner.nextLine();
+        System.out.println("Oh, how interesting. Tell me more!");
+        String reply2 = scanner.nextLine();
+        System.out.println("Thanks for sharing!");
 }
--- part01-Part01_09.Conversation/src/main/java/Conversation.java
+++ part01-Part01_09.Conversation/src/main/java/Conversation.java
@@ -5,8 +5,11 @@ public class Conversation {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
-        // Write your program here
-
+        System.out.println("");
+        System.out.println("Greetings! How are you doing?");
+        String reply1 = scanner.nextLine();
+        System.out.println("Oh, how interesting. Tell me more!");
+        String reply2 = scanner.nextLine();
+        System.out.println("Thanks for sharing!");
     }
 }
