f750fcd0a7e5bb63bbfdc45429b9fa7acfe5e7cd
reeceawalsh <reeceawalsh@hotmail.co.uk>
2022-05-05 17:28:13 +0100

    Part 17 completed

--- part01-Part01_16.SecondsInADay/src/main/java/SecondsInADay.java
+++ part01-Part01_16.SecondsInADay/src/main/java/SecondsInADay.java
@@ -6,7 +6,9 @@ public class SecondsInADay {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        // Write your program here
+        System.out.println("How many days would you like to convert to seconds?");
+        int secondsInADay = Integer.valueOf(scanner.nextLine()) * 24 * 60 * 60;
+        System.out.println(secondsInADay);
 
     }
 }
