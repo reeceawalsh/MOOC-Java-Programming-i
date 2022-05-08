2f7265252f33c5d271bf7cbf88acdc4259a1fa8d
reeceawalsh <reeceawalsh@hotmail.co.uk>
2022-05-08 19:29:40 +0100

    Part 1 completed

--- part02-Part02_01.Squared/src/main/java/Squared.java
+++ part02-Part02_01.Squared/src/main/java/Squared.java
@@ -6,5 +6,9 @@ public class Squared {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        int integer = Integer.valueOf(scanner.nextLine());
+        int squared = integer * integer;
+
+        System.out.println(squared);
     }
 }
