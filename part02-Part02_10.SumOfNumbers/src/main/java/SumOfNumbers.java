de9ec7707a1ac695dbca104d24a51a98c446fe2a
reeceawalsh <reeceawalsh@hotmail.co.uk>
2022-05-08 22:14:21 +0100

    Part 10 completed

--- part02-Part02_10.SumOfNumbers/src/main/java/SumOfNumbers.java
+++ part02-Part02_10.SumOfNumbers/src/main/java/SumOfNumbers.java
@@ -6,5 +6,19 @@ public class SumOfNumbers {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        int sum = 0;
+
+        while (true) {
+            System.out.println("Give a number:");
+            int num = Integer.valueOf(scanner.nextLine());
+
+            if (num == 0) {
+                System.out.println("Sum of the numbers: " + sum);
+                break;
+            }
+
+            sum = sum + num;
+        }
+
     }
 }
