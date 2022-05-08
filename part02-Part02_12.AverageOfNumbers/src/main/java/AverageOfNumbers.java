9866615e653048bf550385dce91e2f74335160bf
reeceawalsh <reeceawalsh@hotmail.co.uk>
2022-05-08 22:58:19 +0100

    Part 12 complete

--- part02-Part02_12.AverageOfNumbers/src/main/java/AverageOfNumbers.java
+++ part02-Part02_12.AverageOfNumbers/src/main/java/AverageOfNumbers.java
@@ -5,6 +5,23 @@ public class AverageOfNumbers {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        double count = 0;
+        double sum = 0;
+
+        while (true) {
+            System.out.println("Give a number:");
+            int num = Integer.valueOf(scanner.nextLine());
+
+            if (num == 0) {
+                double average = sum / count;
+                System.out.println("Average of the numbers: " + average);
+                break;
+            }
+
+            count = count + 1;
+            sum = sum + num;
+
+        }
 
     }
 }
