904908e0e517a10c1d0ca02c841b5ef81ff9b285
reeceawalsh <reeceawalsh@hotmail.co.uk>
2022-05-08 22:19:51 +0100

    Part 11

--- part02-Part02_11.NumberAndSumOfNumbers/src/main/java/NumberAndSumOfNumbers.java
+++ part02-Part02_11.NumberAndSumOfNumbers/src/main/java/NumberAndSumOfNumbers.java
@@ -5,6 +5,22 @@ public class NumberAndSumOfNumbers {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        int count = 0;
+        int sum = 0;
+
+        while (true) {
+            System.out.println("Give a number:");
+            int num = Integer.valueOf(scanner.nextLine());
+
+            if (num == 0) {
+                System.out.println("Number of numbers: " + count);
+                System.out.println("Sum of the numbers: " + sum);
+                break;
+            }
+
+            count = count + 1;
+            sum = sum + num;
+        }
 
     }
 }
