17cebf4ebd9612bf0175ee70f6809c31a3ebc636
reeceawalsh <reeceawalsh@hotmail.co.uk>
2022-05-08 23:02:39 +0100

    Problem with public static void main

--- part02-Part02_13.AverageOfPositiveNumbers/src/main/java/AverageOfPositiveNumbers.java
+++ part02-Part02_13.AverageOfPositiveNumbers/src/main/java/AverageOfPositiveNumbers.java
@@ -6,5 +6,26 @@ public class AverageOfPositiveNumbers {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        double sum = 0;
+        double count = 0;
+
+        while (true) {
+            System.out.println("Give a number:");
+            int num = Integer.valueOf(scanner.nextLine());
+
+            if (num == 0) {
+                double average = sum / count;
+                if (sum > 0) {
+                    System.out.println(average);
+                } else {
+                    System.out.println("Cannot calculate the average");
+                }
+            }
+
+            sum = sum + num;
+            count = count +1;
+
+
+        }
     }
 }
