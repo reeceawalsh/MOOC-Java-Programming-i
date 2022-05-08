ac3985889a61222ee2603b25b73a28a16248c8df
reeceawalsh <reeceawalsh@hotmail.co.uk>
2022-05-08 21:42:00 +0100

    Part 8 completed

--- part02-Part02_08.NumberOfNumbers/src/main/java/NumberOfNumbers.java
+++ part02-Part02_08.NumberOfNumbers/src/main/java/NumberOfNumbers.java
@@ -5,6 +5,17 @@ public class NumberOfNumbers {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        int numberOfNums = 0;
 
+        while (true) {
+            System.out.println("Give a number:");
+            int number = Integer.valueOf(scanner.nextLine());
+
+            if (number == 0) {
+                System.out.println("Number of numbers: " + numberOfNums);
+                break;
+            }
+            numberOfNums = numberOfNums +1;
+        }
     }
 }
