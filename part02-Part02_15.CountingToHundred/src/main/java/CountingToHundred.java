2632c0a63b2ff5dac6c2a4efb9fab39d519ab0e7
reeceawalsh <reeceawalsh@hotmail.co.uk>
2022-05-09 19:09:23 +0100

    Part 15 complete

--- part02-Part02_15.CountingToHundred/src/main/java/CountingToHundred.java
+++ part02-Part02_15.CountingToHundred/src/main/java/CountingToHundred.java
@@ -6,5 +6,10 @@ public class CountingToHundred {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        int input = Integer.valueOf(scanner.nextLine());
+
+        for (int i = input; i <= 100; i++) {
+            System.out.println(i);
+        }
     }
 }
