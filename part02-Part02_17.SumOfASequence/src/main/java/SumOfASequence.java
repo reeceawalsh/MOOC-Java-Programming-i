e02ed159f8e7fef34f315a07c25da967d0238007
reeceawalsh <reeceawalsh@hotmail.co.uk>
2022-05-09 19:24:17 +0100

    Part 17 fini

--- part02-Part02_17.SumOfASequence/src/main/java/SumOfASequence.java
+++ part02-Part02_17.SumOfASequence/src/main/java/SumOfASequence.java
@@ -6,5 +6,13 @@ public class SumOfASequence {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        int sum = 0;
+        System.out.println("Last number?");
+        int lastNum = Integer.valueOf(scanner.nextLine());
+
+        for (int i = 0; i <= lastNum; i++) {
+            sum = sum + i;
+        }
+        System.out.println("The sum is " + sum);
     }
 }
