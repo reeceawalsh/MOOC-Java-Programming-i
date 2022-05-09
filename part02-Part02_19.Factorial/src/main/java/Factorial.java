65a9194bbed25e23ac1fcf82581be52a38f1651b
reeceawalsh <reeceawalsh@hotmail.co.uk>
2022-05-09 19:34:48 +0100

    Part 19 complete

--- part02-Part02_19.Factorial/src/main/java/Factorial.java
+++ part02-Part02_19.Factorial/src/main/java/Factorial.java
@@ -6,5 +6,15 @@ public class Factorial {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        int factorial = 1;
+
+        System.out.println("Give a number:");
+        int input = Integer.valueOf(scanner.nextLine());
+
+        for (int i = 1; i <= input; i++) {
+            factorial = factorial * i;
+        }
+
+        System.out.println("Factorial: " + factorial);
     }
 }
