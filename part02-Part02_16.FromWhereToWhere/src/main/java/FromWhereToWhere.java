b6e30c6a13f0a40577fd064c8983c6e63857a789
reeceawalsh <reeceawalsh@hotmail.co.uk>
2022-05-09 19:16:35 +0100

    Part 16 complete

--- part02-Part02_16.FromWhereToWhere/src/main/java/FromWhereToWhere.java
+++ part02-Part02_16.FromWhereToWhere/src/main/java/FromWhereToWhere.java
@@ -7,5 +7,13 @@ public class FromWhereToWhere {
         Scanner scanner = new Scanner(System.in);
 
         // Write your program here
+        System.out.print("Where to");
+        int upperLimit = Integer.valueOf(scanner.nextLine());
+        System.out.print("Where from");
+        int lowerLimit = Integer.valueOf(scanner.nextLine());
+
+        for (int i = lowerLimit; i <= upperLimit; i++) {
+            System.out.println(i);
+        }
     }
 }
