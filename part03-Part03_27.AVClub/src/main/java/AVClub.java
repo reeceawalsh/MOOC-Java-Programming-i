02b5c7f84f562c71f3636c3243e9446d78004e62
reeceawalsh <reeceawalsh@hotmail.co.uk>
2022-05-22 14:42:26 +0100

    Part 27

--- part03-Part03_27.AVClub/src/main/java/AVClub.java
+++ part03-Part03_27.AVClub/src/main/java/AVClub.java
@@ -6,6 +6,20 @@ public class AVClub {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        while (true) {
+            String input = scanner.nextLine();
+
+            if (input.isEmpty()) {
+                break;
+            } else {
+               String[] split = input.split(" ");
+               for (String word : split) {
+                   if (word.contains("av")) {
+                       System.out.println(word);
+                   }
+               }
+            }
+        }
 
     }
 }
