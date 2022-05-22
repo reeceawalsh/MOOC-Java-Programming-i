953b62dfebefab679e0451bc2d14b600baa3608c
reeceawalsh <reeceawalsh@hotmail.co.uk>
2022-05-22 13:47:28 +0100

    Part 24-26

--- part03-Part03_25.Login/src/main/java/Login.java
+++ part03-Part03_25.Login/src/main/java/Login.java
@@ -6,5 +6,15 @@ public class Login {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        System.out.print("Enter username:");
+        String userName = scanner.nextLine();
+        System.out.print("Enter password:");
+        String password = scanner.nextLine();
+
+        if (userName.equals("emma") && password.equals("haskell") || (userName.equals("alex") && password.equals("sunshine"))) {
+            System.out.println("You have successfully logged in!");
+        } else {
+            System.out.println("Incorrect username or password!");
+        }
     }
 }
--- part03-Part03_26.LineByLine/src/main/java/LineByLine.java
+++ part03-Part03_26.LineByLine/src/main/java/LineByLine.java
@@ -5,7 +5,16 @@ public class LineByLine {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
-
+        while (true) {
+            String input = scanner.nextLine();
+            if (input.isEmpty()) {
+                break;
+            } else {
+                String[] split = input.split(" ");
+            for (String word: split) {
+                    System.out.println(word);
+                }
+            }
+        }
     }
 }
