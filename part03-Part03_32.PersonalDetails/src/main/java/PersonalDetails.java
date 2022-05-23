3358556617adc402fe2cb9a1627ded230b750d78
reeceawalsh <reeceawalsh@hotmail.co.uk>
2022-05-23 19:50:03 +0100

    Complete

--- part03-Part03_32.PersonalDetails/src/main/java/PersonalDetails.java
+++ part03-Part03_32.PersonalDetails/src/main/java/PersonalDetails.java
@@ -6,7 +6,25 @@ public class PersonalDetails {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        String longestName = "";
+        int count = 0;
+        double sumOfAges = 0;
 
+        while (true) {
+            String input = scanner.nextLine();
 
+            if (input.isEmpty()) {
+                System.out.println("Longest name: " + longestName);
+                System.out.println("Average of the birth years: " + (sumOfAges / count));
+                break;
+            } else {
+                String[] inputArray = input.split(",");
+                count ++;
+                sumOfAges += Integer.valueOf(inputArray[1]);
+                if (inputArray[0].length() > longestName.length()) {
+                    longestName = inputArray[0];
+                    }
+                }
+            }
+        }
     }
-}
