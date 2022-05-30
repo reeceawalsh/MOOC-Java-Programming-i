757191641cca81188b49cf4cdd5c955480588e42
reeceawalsh <reeceawalsh@hotmail.co.uk>
2022-05-30 23:43:10 +0100

    Complete

--- part05-Part05_17.Money/src/main/java/MainProgram.java
+++ part05-Part05_17.Money/src/main/java/MainProgram.java
@@ -2,6 +2,12 @@
 public class MainProgram {
 
     public static void main(String[] args) {
-        // test your class here
-    }
+        Money a = new Money(10, 0);
+        Money b = new Money(7, 40);
+
+        Money c = a.minus(b);
+
+        System.out.println(c);  // 6.50e
+        System.out.println(c.euros());
+        }
 }
--- part05-Part05_17.Money/src/main/java/Money.java
+++ part05-Part05_17.Money/src/main/java/Money.java
@@ -23,6 +23,48 @@ public class Money {
         return this.cents;
     }
 
+    public Money plus(Money addition) {
+        Money newMoney = new Money(this.euros + addition.euros, this.cents + addition.cents);
+        return newMoney;
+    }
+
+    public boolean lessThan(Money compared) {
+        if (this.euros < compared.euros) {
+            return true;
+        } else if (this.euros == compared.euros) {
+            if (this.cents < compared.cents) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public Money minus(Money decreaser) {
+        // Find out whether using the decreaser will leave a negative amount, in which case I'm creating an object that is 0,0;
+        int amount1 = 0;
+        int amount2 = 0;
+        amount1 = this.euros * 100 + this.cents;
+        amount2 = decreaser.euros * 100 + decreaser.cents;
+
+        if (amount1 <= amount2) {
+            Money newMoney = new Money(0, 0);
+            return newMoney;
+        }
+        // The new amount will be above 0 so we need to take the euros and cents away from one another.
+            else {
+                if (this.cents < decreaser.cents) { // To account for cents being less than the cents being taken away, this function minuses a euro and adds 100 to the cents to do the calculation.
+                    // I dont have to worry about the cents being less and the euros being less because the first if statement removes that possibility.
+                Money newMoney = new Money((this.euros - 1) - decreaser.euros, (this.cents + 100) - decreaser.cents);
+                return newMoney;
+            }
+            else { // if cents are more then its straight forward.
+                    Money newMoney = new Money(this.euros - decreaser.euros, this.cents - decreaser.cents);
+                    return newMoney;
+                }
+            }
+        }
+
+
     public String toString() {
         String zero = "";
         if (this.cents < 10) {
